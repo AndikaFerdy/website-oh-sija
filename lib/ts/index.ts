fde02e6c02d2e002f752e9f21a11c54c043c2331
Bagas Wahyu Hidayah <bagasdoti69@gmail.com>
2019-04-30 14:20:40 +0700

    add more configurations

--- lib/ts/index.ts
+++ lib/ts/index.ts
@@ -1,5 +1,22 @@
+// @ts-ignore
+require('bootstrap/dist/css/bootstrap.min.css');
+
 import setupServiceWorker from './service-worker';
 
-setupServiceWorker().then(success => {
-  console.log(success);
+window.addEventListener('load', evt => {
+  setupServiceWorker().then(success => {
+    console.log(success);
+  });
+
+  const headTypesetting = document.querySelector(
+    '.head-typesetting'
+  ) as HTMLHeadingElement;
+  const text = 'Welcome';
+
+  let idx = 0;
+  setInterval(() => {
+    if (idx !== text.length) {
+      headTypesetting.textContent += text[idx++];
+    }
+  }, 100);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.sass$/,
+        test: /((.scss)|(.css))$/,
         use: [
           {
             loader: 'style-loader',
@@ -39,7 +39,7 @@ module.exports = {
   },
 
   resolve: {
-    extensions: ['.ts'],
+    extensions: ['.ts', '.js', '.jsx'],
   },
 
   plugins: [
