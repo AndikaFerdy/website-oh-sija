ae80fdf9cc875d834f5cd805b4cd90d724a18a5b
Bagas Wahyu Hidayah <bagasdoti69@gmail.com>
2019-05-01 20:45:23 +0700

    mbuh

---
+++ lib/ts/ui/animation.ts
@@ -0,0 +1,11 @@
+function showFromCenter(images: HTMLImageElement[]) {
+  images[4].classList.add('show');
+  images[5].classList.add('show');
+  images[3].classList.add('show');
+  images[7].classList.add('show');
+  images[1].classList.add('show');
+  images[2].classList.add('show');
+  images[8].classList.add('show');
+  images[6].classList.add('show');
+  images[0].classList.add('show');
+}
